51b8b76168e685dd922c83bf14da4e198bb05fdb
Maksim Yurau <2004softwaredeveloper@gmail.com>
2020-12-23 21:22:10 +0300

    Project update. Updated MainController.java. Updated pom.xml.

--- src/main/java/by/maksim/controller/MainController.java
+++ src/main/java/by/maksim/controller/MainController.java
@@ -1,4 +1,4 @@
-package by.maksim.controllers;
+package by.maksim.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,9 +7,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
 
-    @GetMapping ("/")
-    public String home (Model model) {
+    @GetMapping("/")
+    public String home(Model model) {
         model.addAttribute("title", "Главная страница");
+        return "home";
+    }
+
+    @GetMapping("/about")
+    public String about(Model model) {
+        model.addAttribute("title", "Об авторе");
         return "home";
     }
 }
