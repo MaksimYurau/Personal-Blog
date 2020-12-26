f3e30a23816bf16b37d636c6fedc53cc969d929e
Maksim Yurau <2004softwaredeveloper@gmail.com>
2020-12-26 20:27:04 +0300

    Project update. Updated BlogController.java. Updated blog-main.html.

--- src/main/java/by/maksim/controller/BlogController.java
+++ src/main/java/by/maksim/controller/BlogController.java
@@ -1,5 +1,8 @@
 package by.maksim.controller;
 
+import by.maksim.domain.Post;
+import by.maksim.repository.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,9 +10,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class BlogController {
 
+    @Autowired
+    private PostRepository postRepository;
+
     @GetMapping("/blog")
     public String blogMain(Model model) {
-        model.addAttribute("title", "Блог");
+        Iterable<Post> posts = postRepository.findAll();
+        model.addAttribute("posts", posts);
         return "blog-main";
     }
 }
