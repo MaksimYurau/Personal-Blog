24677bbb6e88ead1ed748854b9ea759e9bdd5c69
Maksim Yurau <2004softwaredeveloper@gmail.com>
2020-12-30 18:22:25 +0300

    Project update. Updated PostRepository.java.

--- src/main/java/by/maksim/repository/PostRepository.java
+++ src/main/java/by/maksim/repository/PostRepository.java
@@ -3,6 +3,8 @@ package by.maksim.repository;
 import by.maksim.domain.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PostRepository extends JpaRepository<Post, Long> {
+import java.util.List;
 
+public interface PostRepository extends JpaRepository<Post, Long> {
+    List<Post> findByUserId(Long userId);
 }
