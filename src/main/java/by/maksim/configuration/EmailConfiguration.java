ff9c9a71c700cf8a828a58b6e83ec5e1a11d422c
Maksim Yurau <2004softwaredeveloper@gmail.com>
2021-01-02 19:48:52 +0300

    Project update. Added FeedBack.java. Added ContactService.java. Added ContactServiceImpl.java. Added EmailConfiguration.java. Added FeedBackController.java. Added test.http. Updated MailConfiguration.java. Updated MyConstants.java. Updated ContactController.java.

---
+++ src/main/java/by/maksim/configuration/EmailConfiguration.java
@@ -0,0 +1,27 @@
+package by.maksim.configuration;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+@Configuration
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class EmailConfiguration {
+
+    @Value("${spring.mail.host}")
+    private String host;
+
+    @Value("${spring.mail.port}")
+    private int port;
+
+    @Value("${spring.mail.username}")
+    private String username;
+
+    @Value("${spring.mail.password}")
+    private String password;
+}
--- src/main/java/by/maksim/controller/ContactController.java
+++ src/main/java/by/maksim/controller/ContactController.java
@@ -1,22 +1,40 @@
-package by.maksim.controller;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-
-@Controller
-public class ContactController {
-
-    @Qualifier("getJavaMailSender")
-    @Autowired
-    private JavaMailSender javaMailSender;
-
-    @GetMapping("/contact")
-    public String contact() {
-        return "contact";
-    }
-
-
-}
+//package by.maksim.controller;
+//
+//import by.maksim.domain.Contact;
+//import by.maksim.service.ContactService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Qualifier;
+//import org.springframework.mail.javamail.JavaMailSender;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.ModelMap;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.ModelAttribute;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//
+//@Controller
+//@RequestMapping("/contact")
+//public class ContactController {
+//
+//    @Autowired
+//    private ContactService contactService;
+//
+//    @Qualifier("getJavaMailSender")
+//    @Autowired
+//    private JavaMailSender javaMailSender;
+//
+//    @GetMapping
+//    public String contact(ModelMap modelMap) {
+//        modelMap.put("contact", new Contact());
+//        return "contact";
+//    }
+//
+//    @PostMapping(value = "send")
+//    public String send(@ModelAttribute("contact") Contact contact, ModelMap modelMap) throws Exception {
+//        String content = "Name: " + contact.getName();
+//        content += "<br>Phone: " + contact.getPhone();
+//        content += "<br>Address: " + contact.getAddress();
+//        contactService.send(contact.getEmail(), "2004softwaredeveloper@gmail.com", contact.getSubject(), content);
+//        return "contact";
+//    }
+//}
