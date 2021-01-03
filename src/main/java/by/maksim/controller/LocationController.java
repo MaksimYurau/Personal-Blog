062f0fc3a98e62493606596c2d708f7a3a4b3be0
Maksim Yurau <2004softwaredeveloper@gmail.com>
2021-01-03 18:25:46 +0300

    Project update. Added LocationController.java. Added location.html. Updated about.html. Updated blog-add.html. Updated blog-details.html. Updated blog-edit.html. Updated blog-main.html. Updated contact.html. Updated home.html. Updated login.html. Updated privacy.html. Updated registration.html. Updated terms.html.

---
+++ src/main/java/by/maksim/controller/LocationController.java
@@ -0,0 +1,13 @@
+package by.maksim.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LocationController {
+
+    @GetMapping("/location")
+    public String location() {
+        return "location";
+    }
+}
