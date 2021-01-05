bd502ecba87a6f20a76788b45a990c401e60a977
Maksim Yurau <2004softwaredeveloper@gmail.com>
2021-01-05 16:32:42 +0300

    Project update. Updated MvcConfiguration.java. Updated login.html. Updated registration.html.

--- src/main/java/by/maksim/configuration/MvcConfiguration.java
+++ src/main/java/by/maksim/configuration/MvcConfiguration.java
@@ -18,6 +18,7 @@ public class MvcConfiguration implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
         registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
     }
 }
 
